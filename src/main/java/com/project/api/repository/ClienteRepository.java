1f84b4a6f0cc142212178de7b00c02c772f26c46
AndreCustodioL <andre.custodio@outlook.com>
2023-11-20 10:57:27 -0300

    Remoção de arquivos duplicados

--- src/main/java/com/project/api/repository/ClienteRepository.java
+++ src/main/java/com/project/api/repository/ClienteRepository.java
@@ -1,6 +1,5 @@
 package com.project.api.repository;
 
-import com.project.api.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
--- src/main/java/com/project/api/repository/ProdutoRepository.java
+++ src/main/java/com/project/api/repository/ProdutoRepository.java
@@ -1,7 +1,6 @@
 package com.project.api.repository;
 
 
-import com.project.api.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
